c74d770739f6d254626726d791b80a9ca1428257
zhangjiawei <48988840@qq.com>
2022-10-24 00:14:36 +0800

    修复查询 不定位到第一页问题

--- src/components/dawer/TeacherDawer.vue
+++ src/components/dawer/TeacherDawer.vue
@@ -7,7 +7,7 @@
             <el-input v-model="searchInfo.name" @keyup.enter.native="getTableData()" placeholder="请输入教师姓名" />
           </el-form-item>
           <el-form-item>
-            <el-button size="mini" type="primary" icon="el-icon-search" @click="getTableData()">查询</el-button>
+            <el-button size="mini" type="primary" icon="el-icon-search" @click="getTableData(1)">查询</el-button>
           </el-form-item>
         </el-form>
       </div>
